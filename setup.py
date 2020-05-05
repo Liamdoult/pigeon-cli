a7307de04de5aeeb7a5172a6fe810edd577a1ea6
LiamDoult <3579535@myuwc.ac.za>
2020-05-05 17:57:14 +0100

    Add testing framework

--- setup.py
+++ setup.py
@@ -1,18 +1,28 @@
 """ Basic setup for Pigeon CLI """
 from os import path
-from setuptools import setup
+from setuptools import setup, find_packages
 
 here = path.abspath(path.dirname(__file__))
 
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+test_dependencies = ['pytest']
+
 setup(
     name='pigeon-cli',
     version='0.0.2',
     aution='Liam Doult',
+    packages=find_packages(),
+    python_requires='>=3.7, < 4',
     install_requires=[
         'click',
         'requests',
     ],
+    tests_require=test_dependencies,
+    setup_requires=['pytest-runner'],
+    extras_require={
+        'test': test_dependencies,
+    },
+    test_suite='test',
 )
---
+++ test/test_framework.py
@@ -0,0 +1,7 @@
+""" Testing the tests ;p """
+import pytest
+
+
+def test_python():
+    """ Test that the test suite works correctly """
+    assert True
